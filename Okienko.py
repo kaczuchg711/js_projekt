648f537a3256cb9e736e01d9e9de323e487e48ab
Tomasz Kacza ll <kaczatom4@gmail.com>
2019-04-20 17:32:50 +0200

    In Window 	add in init and del nr_set support

--- Okienko.py
+++ Okienko.py
@@ -1,8 +1,19 @@
 class Window:
+    nr_set = set()
+
     def __init__(self, n, a=False, b=False):
-        self._nr = n
-        self._a = a
-        self._b = b
+        """init and add window number in nr_set"""
+        if n in Window.nr_set:
+            raise ValueError
+        else:
+            Window.nr_set.add(n)
+            self._nr = n
+            self._a = a
+            self._b = b
+    def __del__(self):
+        """remove window number in nr_set"""
+        print(self.nr)
+        Window.nr_set.remove(self._nr)
 
     @property
     def nr(self):
@@ -28,6 +39,5 @@ class Window:
     def b(self, b):
         self._b = b
 
-
-w = Window(1, True, False)
-print(w.a)
+w1 = Window(1, True, False)
+w2 = Window(2, True, False)
